2625a392399847bd55aa5f466aaba72816a9dfbe
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2026-01-12 17:12:28 -0500

    Add files via upload

---
+++ zoe's website/js/main.js
@@ -0,0 +1,22 @@
+//lightbox
+(() => {
+const lightBox = document.querySelector("#lightbox");
+const lightBoxTrigger = document.querySelector("#hamburger");
+const closeBtn = document.querySelectorAll('#close-button, .lightboxLink');
+
+function openLightbox() {
+  console.log('open lightbox')
+  lightBox.style.display = 'flex';
+}
+
+function closeLightbox() {
+  console.log('close lightbox')
+  lightBox.style.display = 'none';
+}
+
+lightBoxTrigger.addEventListener('click', openLightbox);
+closeBtn.forEach(Btn => {
+Btn.addEventListener('click', closeLightbox);
+});
+
+})();
