f29babb07a0b0863170ee3e73c63ffa0a871de50
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2026-01-22 21:54:09 -0500

    experimental code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+(() => {
+const tl = gsap.timeline({ defaults: { ease: "power3.out" } });
+
+// 1. Logo eases left
+tl.to("#logo", {
+  x: -120,
+  duration: 1
+});
+
+// 2. Text slides out from behind logo
+tl.to("#welcome", {
+  x: 80,
+  opacity: 1,
+  duration: 1
+}, "-=0.6");
+
+// 3. Pause briefly
+tl.to({}, { duration: 1 });
+
+// 4. Lens flare wipe
+tl.to("#wipe", {
+  scaleX: 1,
+  duration: 0.8,
+  ease: "power4.in"
+});
+
+// 5. Reveal site and remove intro
+tl.set("#intro", { display: "none" });
+tl.to("#site", { opacity: 1, duration: 0.4 });
+})();
+
 //lightbox
 (() => {
 const lightBox = document.querySelector("#lightbox");
@@ -20,3 +51,4 @@ Btn.addEventListener('click', closeLightbox);
 });
 
 })();
+
